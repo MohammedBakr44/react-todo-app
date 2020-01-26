8db44b6b3053939452c5f72fffbf9dc0e21c9714
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2020-01-26 05:20:20 +0200

    Local storage added.

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default class App extends Component {
         this.setState({
           todoItems: [...this.state.todoItems,
           {action: task, done: false}],
-        });
+        }, () => localStorage.setItem("todos", JSON.stringify(this.state)));
       }
   }
 
@@ -43,6 +43,20 @@ export default class App extends Component {
 
   todoTableRows = (doneValue) => this.state.todoItems.filter(item => item.done === doneValue).map(item => 
     <TodoRow key={ item.action } item={item} callback={this.toggleTodo} />);
+
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null
+        ? JSON.parse(data)
+        :  {
+            userName: "Adam",
+            todoItems: [{ action: "Buy Flowers", done: false },
+                        { action: "Get Shoes", done: false },
+                        { action: "Collect Tickets", done: true },
+                        { action: "Call Joe", done: false }],
+            showCompleted: true
+        });
+  }  
 
   changeStateData = () => {
     this.setState({
